fbc487a2ccb1930261123cb2089ae0e1e8cf0dd9
mattu23 <hirokiim.27@gmail.com>
2023-07-10 22:56:11 +0900

    inputCheck.js内の処理の説明を追記

--- inputCheck.js
+++ inputCheck.js
@@ -18,7 +18,11 @@ function inputCheck() {
   //選択した収支のラジオボタンの取得
   let radio = document.getElementsByName("balance");
   let balance;
+  //受け取ったラジオボタンの配列データを、チェックボックスにチェックが入っているか、順番に確認する処理
+  //このｆorの条件文内の「radio.length」は配列の数を指している
   for(let i= 0; i<radio.length; i++) {
+   //受け取ったラジオボタンの配列データが、何番目の配列がチェックボックスにチェック入っているかが判明する
+   //ｎ番目の配列データがtrueの時は処理が終わるようbreakを記載
     if(radio[i].checked == true) {
       balance = radio[i].value;
       break;
@@ -26,6 +30,8 @@ function inputCheck() {
   }
 
    //日付、カテゴリ、金額、メモの取得
+   //getElementByIdの引数に取り出したいimputのidを設定することで値を取り出せる
+   //このあとの処理で使用するため、取り出した値を各変数に格納している
    let date = document.getElementById("date").value;
    let category = document.getElementById("category").value;
    let amount = document.getElementById("cost").value;
@@ -35,6 +41,7 @@ function inputCheck() {
    if(date == ""){
       result = false;
       alert("日付が未記入です");
+      //&&で支出も含めないと、収入のラジオボタン時の初期値と混ざってしまって正常に判断できない
    } else if (category == "-選択してください-" && balance == "支出") {
       result = false;
       alert("カテゴリを選択してください");
@@ -45,7 +52,8 @@ function inputCheck() {
       result = false;
       alert("メモが未記入です");
    }
+   //上記4つがどれか1つでも当てはまればfalseを返す。
+   //最後にreturnを返すことで、falseの場合、データの登録処理を中断する関数が実装される
    return result;
 }
 
-//完成！
